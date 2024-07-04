b71de7fbb4f7ba81da89dff6665e9c52fd019424
Tomshr29 <tomscherer29@gmail.com>
2024-07-04 21:35:50 +0200

    clean up

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -1,23 +1,16 @@
 import Post from "#models/post";
 import type { HttpContext } from "@adonisjs/core/http";
-import vine from "@vinejs/vine";
 
 export default class DashboardController {
-  static validator = vine.compile(
-    vine.object({
-      fullName: vine.string().trim(),
-      email: vine.string().email().trim(),
-    }),
-  );
-
   async index({ auth, inertia, response }: HttpContext) {
-    const authUser = auth.user!;
+    const user = auth.user!;
 
-    if (!authUser) {
+    if (!user) {
       return response.redirect().toPath("/login");
     }
+
     const posts = await Post.query()
-      .where("user_id", authUser.id)
+      .where("user_id", user.id)
       .orderBy("created_at", "desc");
     return inertia.render("dashboard/index", { posts });
   }
